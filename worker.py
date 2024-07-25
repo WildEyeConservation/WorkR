d21f367b052458be659278547ae3deaf63e59532
nicholasosner <nicholasosner@gmail.com>
2024-07-25 12:07:49 +0000

    Celery acks_late

--- worker.py
+++ worker.py
@@ -37,7 +37,7 @@ import boto3
 import json
 
 REDIS_IP = os.environ.get('REDIS_IP') or '127.0.0.1'
-app = Celery('WorkR', broker='redis://'+REDIS_IP,backend='redis://'+REDIS_IP,broker_transport_options={'visibility_timeout': 86400},result_expires=86400,task_acks_late=True)
+app = Celery('WorkR', broker='redis://'+REDIS_IP,backend='redis://'+REDIS_IP,broker_transport_options={'visibility_timeout': 259200},result_expires=259200,task_acks_late=False,worker_prefetch_multiplier=1)
 workername="default"
 
 s3client = boto3.client('s3')
